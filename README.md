9483d41e31338430bb2398a465871e3820ebcc72
RaulLiberato <raulliberato547@gmail.com>
2021-11-09 21:41:23 -0300

    meu projeto

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hardesafi
+Um site que cria listas de desafio, feito em Html, Css e Java Script para treinamento
---
+++ scripts/login.js
@@ -0,0 +1,26 @@
+var form = document.forms["formularioLogin"]
+
+//eventos apos o botão de login ser apertado
+document.getElementById("logIn").addEventListener("click",pegaValores);
+
+function pegaValores(){
+
+    var formData = new FormData(form)
+
+    var usuario = {}
+    
+    for (var i of formData.entries()) {
+        usuario[i[0]] = i[1]
+        console.log(i[1])
+    }
+    verificaNulo(usuario)
+}
+
+//valida as inputs
+function verificaNulo(usuario){
+    form.classList.add('was-validated')
+
+    if(usuario.email == "raul@gmail.com" && usuario.senha == "Batatadoce15"){
+        window.location.href = "https://www.google.com.br/"
+    }
+}
